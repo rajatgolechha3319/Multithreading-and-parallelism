a0f564088824ea45f3873ff5b97208cfaa128bb7
Rajat Golechha <98651238+rajatgolechha3319@users.noreply.github.com>
2023-02-15 22:47:16 +0530

    Update mythread.h

--- Assignment2-COP290/part2/include/mythread.h
+++ Assignment2-COP290/part2/include/mythread.h
@@ -8,33 +8,35 @@
 #include<sys/time.h>
 #include<unistd.h>
 
-void mythread_init();      // Initialize threads list
+void mythread_init(){
 
+};      // Initialize threads list
+
+ucontext_t* mythread_self(){
+// return the address of the running context
+
+};  
 ucontext_t* mythread_create(void func(void*), void* arg);  // Create a new thread
-
 void mythread_join();  // Waits for other thread to complete. It is used in case of dependent threads.
-
 void mythread_yield();  // Perform context switching here
 
 struct lock {
 	ucontext_t* ctx;
 };
-
-struct lock* lock_new()   // return an initialized lock object
-{
-    struct lock* lk = (struct lock*) malloc(sizeof (struct lock*));
-    lk->ctx = NULL;
-    return lk;
+struct lock* lock_new(){   // return an initialized lock object
+	struct lock* lk = (struct lock*)malloc(sizeof(struct lock));
+	lk->ctx = NULL;
+	return lk;
 }
-
-void lock_acquire(struct lock* lk)   // Set lock. Yield if lock is acquired by some other thread.
-{
-
-}
-
-int lock_release(struct lock* lk)   // Release lock
-{
-    lk->ctx = NULL;
-}
+void lock_acquire(struct lock* lk){   // Set lock. Yield if lock is acquired by some other thread.
+	while(lk->ctx != NULL){
+		mythread_yield();
+	}
+	lk->ctx = mythread_self(); // incomplete
+};
+int lock_release(struct lock* lk){   // Release lock
+	lk->ctx = NULL;
+	return 0;
+};
 
 #endif
