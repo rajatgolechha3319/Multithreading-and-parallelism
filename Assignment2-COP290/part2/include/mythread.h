54609e4f84b6056ceb84d5dcd65184897fc04416
Devang Garg <97738799+devanggarg2003@users.noreply.github.com>
2023-02-15 21:41:54 +0530

    Update mythread.h

--- Assignment2-COP290/part2/include/mythread.h
+++ Assignment2-COP290/part2/include/mythread.h
@@ -9,15 +9,32 @@
 #include<unistd.h>
 
 void mythread_init();      // Initialize threads list
+
 ucontext_t* mythread_create(void func(void*), void* arg);  // Create a new thread
+
 void mythread_join();  // Waits for other thread to complete. It is used in case of dependent threads.
+
 void mythread_yield();  // Perform context switching here
 
 struct lock {
 	ucontext_t* ctx;
 };
-struct lock* lock_new();   // return an initialized lock object
-void lock_acquire(struct lock* lk);   // Set lock. Yield if lock is acquired by some other thread.
-int lock_release(struct lock* lk);   // Release lock
+
+struct lock* lock_new()   // return an initialized lock object
+{
+    struct lock* lk = (struct lock*) malloc(sizeof (struct lock*));
+    lk->ctx = NULL;
+    return lk;
+}
+
+void lock_acquire(struct lock* lk)   // Set lock. Yield if lock is acquired by some other thread.
+{
+
+}
+
+int lock_release(struct lock* lk)   // Release lock
+{
+    lk->ctx = NULL;
+}
 
 #endif
