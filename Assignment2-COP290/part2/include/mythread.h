24f3f498ba33915db7af2d7ff7064f6569574415
Devang Garg <97738799+devanggarg2003@users.noreply.github.com>
2023-02-16 15:18:46 +0530

    Update mythread.h

--- Assignment2-COP290/part2/include/mythread.h
+++ Assignment2-COP290/part2/include/mythread.h
@@ -1,42 +1,85 @@
 #ifndef THREAD_H
 #define THREAD_H
-
+#define _XOPEN_SOURCE 600
 #include<stdio.h>
 #include<stdlib.h>
 #include<ucontext.h>
 #include<signal.h>
 #include<sys/time.h>
 #include<unistd.h>
+#include<list.h>
 
-void mythread_init(){
+struct list *thread_list;
+ucontext_t joiner;
+struct listentry* current_ctx;
 
-};      // Initialize threads list
+void mythread_init()      // Initialize threads list
+{
+    thread_list = list_new();
+    current_ctx = thread_list->head;
+}
 
-ucontext_t* mythread_self(){
-// return the address of the running context
 
-};  
-ucontext_t* mythread_create(void func(void*), void* arg);  // Create a new thread
-void mythread_join();  // Waits for other thread to complete. It is used in case of dependent threads.
-void mythread_yield();  // Perform context switching here
+
+ucontext_t* mythread_create(void func(void*), void* arg)  // Create a new thread
+{
+    ucontext_t *mtc = (ucontext_t *)malloc(sizeof (ucontext_t *));
+    getcontext(mtc);
+    char st[8192];
+    mtc->uc_stack.ss_sp = st;
+    mtc->uc_stack.ss_size = sizeof st;
+    mtc->uc_link = &joiner;
+    makecontext(mtc, func, 1, arg);
+    list_add(thread_list, &mtc);
+    return mtc;
+}
+
+
+void mythread_join()  // Waits for other thread to complete. It is used in case of dependent threads.
+{
+//    ucontext_t self;
+    struct listentry *t = thread_list->head;
+    while (t != thread_list->tail){
+        current_ctx = t;
+//        ucontext_t &p = *t->data;
+        swapcontext(&joiner, t->data); // to improve
+        t = t->next;
+    }
+    swapcontext(&joiner, t->data); // to improve
+}
+
+void mythread_yield()  // Perform context switching here
+{
+    if(current_ctx == thread_list->tail){
+        current_ctx = thread_list->head;
+    } else{
+        current_ctx = current_ctx->next;
+    }
+    ucontext_t sel;
+    swapcontext(&sel, current_ctx->data); // to improve
+}
 
 struct lock {
 	ucontext_t* ctx;
 };
-struct lock* lock_new(){   // return an initialized lock object
-	struct lock* lk = (struct lock*)malloc(sizeof(struct lock));
-	lk->ctx = NULL;
-	return lk;
-}
-void lock_acquire(struct lock* lk){   // Set lock. Yield if lock is acquired by some other thread.
-	while(lk->ctx != NULL){
-		mythread_yield();
-	}
-	lk->ctx = mythread_self(); // incomplete
-};
-int lock_release(struct lock* lk){   // Release lock
-	lk->ctx = NULL;
-	return 0;
-};
 
+struct lock* lock_new()   // return an initialized lock object
+{
+    struct lock* lk = (struct lock*) malloc(sizeof (struct lock*));
+    lk->ctx = NULL;
+    return lk;
+}
+
+void lock_acquire(struct lock* lk)   // Set lock. Yield if lock is acquired by some other thread.
+{
+    while (lk->ctx != NULL){
+        mythread_yield();
+    }
+    lk->ctx = current_ctx->data;
+}
+
+int lock_release(struct lock* lk)   // Release lock
+{
+    lk->ctx = NULL;
+    return 0;}
 #endif
